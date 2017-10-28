0b7dcab05cc1c3dfc3bb0f42725afd6d1b3247a7
Sahidul Hassan <alrazon@gmail.com>
2017-10-28 21:49:14 +0600

    project import

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CSVToJSON
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 89);
+            this.panel1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "CSV";
+            this.openFileDialog1.FileName = "*.csv";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "*.json";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(81, 107);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(293, 22);
+            this.txtPath.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CSV File";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::CSVToJSON.Properties.Resources.cancel;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(228, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 33);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Close";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Image = global::CSVToJSON.Properties.Resources.active;
+            this.btnConvert.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvert.Location = new System.Drawing.Point(150, 146);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(70, 33);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Export";
+            this.btnConvert.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Image = global::CSVToJSON.Properties.Resources.folder;
+            this.btnOpen.Location = new System.Drawing.Point(382, 102);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(45, 33);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::CSVToJSON.Properties.Resources._1368824358_Arrow_Right;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(92, 18);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 43);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::CSVToJSON.Properties.Resources.CSV;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 76);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::CSVToJSON.Properties.Resources.JSON;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(147, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(99, 76);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CSV to JSON Convertor";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CSVToJSON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+            txtPath.Text = openFileDialog1.FileName;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileName = txtPath.Text;
+                if (fileName != string.Empty)
+                {
+                    string[] FileExt = fileName.Split('.');
+                    string FileEx = FileExt[FileExt.Length - 1];
+                    if (FileEx.ToLower() == "csv")
+                    {
+                        saveFileDialog1.ShowDialog(this);
+                        var destPath = saveFileDialog1.FileName;
+                        
+                        StreamWriter sw = new StreamWriter(destPath);
+                        var csv = new List<string[]>();
+                        var lines = System.IO.File.ReadAllLines(fileName);
+
+                        var txt = System.IO.File.ReadAllText(fileName);
+                        var value = CsvToJson(txt);
+                      
+                        var json = string.Empty;
+                        foreach (var s in value)
+                        {
+                            json += s;
+                            json += ",";
+                        }
+                        if (json.Length > 0)
+                        {
+                            json = json.Substring(0, json.Length - 1);
+                        }
+
+                        json = "{ \"data\":[" + json + "]}";
+                        sw.Write(json);
+                        sw.Close();
+                        MessageBox.Show("File is converted to json.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid File");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("File Not Found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            } 
+        }
+
+        public List<string> CsvToJson(string value)
+        {
+            // Get lines.
+            if (value == null) return null;
+            string[] lines = value.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 2) throw new InvalidDataException("Must have header line.");
+
+            // Get headers.
+            string[] headers = lines[0].Split(',');
+            for (int h = 0; h < headers.Length; h++)
+            {
+                if (headers[h].Trim().ToLower() == "req qty")
+                    headers[h] = "req_qty";
+                else if (headers[h].Trim().ToLower() == "item name")
+                    headers[h] = "item_name";
+                else if (headers[h].Trim().ToLower() == "item number")
+                    headers[h] = "item_number";
+            }
+            // Build JSON array.
+            var csv = new List<string>();
+           
+            for (int i = 1; i < lines.Length; i++)
+            {
+                //string[] fields = lines[i].Split(',');
+                string lineText = lines[i];
+                lineText = lineText.Replace("\"\"", ";;");
+
+                try
+                {
+                    int start = 0;
+                    while (true)
+                    {
+                        if (i == 9671)
+                        {
+                            i = i;
+                        }
+                        start = lineText.IndexOf("\"", start);
+                        if (start == -1) break;
+                        string first = lineText.Substring(0, start);
+                        int end = lineText.IndexOf("\"", start + 1);
+                        if (end == -1)
+                        {
+                            //first = lines[i];
+                            break;
+                        }
+                        string last = lineText.Substring(end);
+                        lineText = lineText.Substring(start, end - start);
+                        int count = lineText.Split(',').Length - 1;
+                        lineText = lineText.Replace(",", "##");
+                        lineText = first + lineText + last;
+
+                        start = end + 2 + count;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                
+                string[] fields = lineText.Split(',');
+                if (i == 21000) {
+                    i = i;
+                }
+                if (fields.Length != headers.Length) throw new InvalidDataException("Field count must match header count."+i);
+                var jsonElements = string.Empty;
+                for (int x = 0; x < fields.Length; x++)
+                {
+                    fields[x] = fields[x].Replace(";;", "\"\"");
+                    //jsonElements += "" + headers[x] + "" + ":" + "" + fields[x] + "";
+                    jsonElements += "\"" + headers[x] + "\"" + ":" + "\"" + fields[x] + "\"";
+                    jsonElements += ",";
+                }
+                if (jsonElements.Length > 0)
+                {
+                    jsonElements = jsonElements.Replace("##", ",");
+                    jsonElements = jsonElements.Substring(0, jsonElements.Length - 1);
+                }
+                csv.Add("{" + jsonElements + "}");
+            }
+           
+            return csv;
+        }
+        
+
+    }
+}
